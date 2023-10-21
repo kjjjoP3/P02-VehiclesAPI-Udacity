2e8766c9dfcb5582fc0a5d94ffcd7dbd16d43d1a
ThanhBT12 <thanhvlok0905@outlook.com.vn>
2023-10-21 18:25:31 +0700

    update carcontroller

--- vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
@@ -81,8 +81,8 @@ class CarController {
          * TODO: Use the `assembler` on that saved car and return as part of the response.
          *   Update the first line as part of the above implementing.
          */
-        car = carService.save(car);
-        Resource<Car> resource = assembler.toResource(car);
+        Car savedCar = carService.save(car);
+        Resource<Car> resource = assembler.toResource(savedCar);
         return ResponseEntity.created(new URI(resource.getId().expand().getHref())).body(resource);
     }
 
@@ -101,8 +101,8 @@ class CarController {
          *   Update the first line as part of the above implementing.
          */
         car.setId(id);
-        car = carService.save(car);
-        Resource<Car> resource = assembler.toResource(car);
+        Car savedCar = carService.save(car);
+        Resource<Car> resource = assembler.toResource(savedCar);
         return ResponseEntity.ok(resource);
     }
 
